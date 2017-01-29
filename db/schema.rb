19a7cb30e066dbcf93ad13f247bd037884c17aed
Cyril Ferte <cyril@fertec.fr>
2017-01-29 12:27:02 +0000

    color calendar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128154617) do
+ActiveRecord::Schema.define(version: 20170129100729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,10 @@ ActiveRecord::Schema.define(version: 20170128154617) do
     t.float    "impact"
     t.float    "fiab"
     t.date     "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "lv"
+    t.string   "description"
   end
 
   create_table "users", force: :cascade do |t|
